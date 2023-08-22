04f1b09545175cb0debe9a4ebc4b367750213efc
German Personal <german.prt@gmail.com>
2023-08-22 08:42:55 -0500

    Adding initial projects

---
+++ packages/logout/src/app/app.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="logout" />
+
+      <div />
+    </>
+  );
+}
+
+export default App;

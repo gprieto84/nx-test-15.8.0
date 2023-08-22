8a2cf2ea13e4b358261c6658979816c899c5a690
German Personal <german.prt@gmail.com>
2023-08-22 09:30:58 -0500

    Add mod to admin

--- packages/admin/src/app/app.tsx
+++ packages/admin/src/app/app.tsx
@@ -6,7 +6,7 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <>
-      <NxWelcome title="admin" />
+      <NxWelcome title="admin - NEw" />
 
       <div />
     </>
